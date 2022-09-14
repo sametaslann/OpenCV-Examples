6227227725db34236a2f3715215649e822703b55
sametaslann <abdulsamed418@gmail.com>
2022-09-14 09:18:12 +0300

    s

--- CraneAlignment.py
+++ CraneAlignment.py
@@ -38,18 +38,19 @@ while True:
         
         for (x,y,r) in circles:
             radius = r
-            horizontalDist = 10 / pow(2,radius-1)
-            verticalDistance = math.sqrt(abs(320-x)*abs(320-x) + abs(230-y)*abs(230-y)) * (0.3)
+            horizontalDist = 1500/r
+            verticalDistance = 20/(horizontalDist) * math.sqrt(abs(320-x)*abs(320-x) + abs(230-y)*abs(230-y))
             print("Horizontal: " , horizontalDist)
             print("vertical: " , verticalDistance)
             
             
-            pertencage = np.arctan(verticalDistance/horizontalDist)
-            finalStr = "%" + str(pertencage)
+            pertencage = (180/(22/7))*np.arctan(verticalDistance/horizontalDist)
+            pertencage = round(pertencage,2)
+            finalStr = str(pertencage) + "degree"
             
             
-            cv.line(img=original, pt1=(320, 230), pt2=(x, y), color=(255, 0, 0), thickness=3, lineType=8, shift=0)
-            cv.putText(original,str(finalStr),(20,20), cv.FONT_HERSHEY_SIMPLEX,1,(255,255,255),thickness=2)
+            cv.line(img=original, pt1=(320, 230), pt2=(x, y), color=(255, 0, 0), thickness=3, lineType=5, shift=0)
+            cv.putText(original,str(finalStr),(30,30), cv.FONT_HERSHEY_SIMPLEX,1,(255,255,255),thickness=2)
             
             if x<330+radius and x>310-radius and y>220-radius and y<240+radius:
                 print(circles)
